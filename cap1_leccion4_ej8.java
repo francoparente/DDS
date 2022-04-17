46881fe5e06341ef7fadce72aa0d0c9f255c22a3
Franco Parente <fparente14@frba.utn.edu.ar>
2022-04-17 03:05:52 -0300

    add ej8

---
+++ cap1_leccion4_ej8.java
@@ -0,0 +1,50 @@
+import java.util.stream.Collectors;
+
+class Repertorio {
+  List<Chiste> chistes;
+
+  Repertorio(List<Chiste> chistes) {
+    this.chistes = chistes;
+  }
+
+  long cantidadBuenosChistes() {
+    return chistes.stream().filter(chiste -> chiste.esBueno()).count();
+  }
+
+  boolean todosBuenos() {
+    return chistes.stream().allMatch(chiste -> chiste.esBueno());
+  }
+
+  List<Chiste> chistesLargos() {
+    return chistes.stream().filter(chiste -> chiste.largo() > 50).collect(Collectors.toList());
+  }
+
+  List<String> textoDeLosChistes() {
+    return chistes.stream().map(chiste -> chiste.getTexto()).collect(Collectors.toList());
+  }
+}
+
+// biblioteca
+/*
+ * class Chiste {
+ * String texto;
+ * boolean bueno;
+ * 
+ * Chiste(String texto, boolean bueno) {
+ * this.texto = texto;
+ * this.bueno = bueno;
+ * }
+ * 
+ * int largo() {
+ * return texto.length();
+ * }
+ * 
+ * String getTexto() {
+ * return texto;
+ * }
+ * 
+ * boolean esBueno() {
+ * return bueno;
+ * }
+ * }
+ */
